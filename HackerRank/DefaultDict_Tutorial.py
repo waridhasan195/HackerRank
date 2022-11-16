c1fce9561f2254ee09441ad85d8cef99a52c1cdb
waridhasan195 <waridhasan195@gmail.com>
2022-11-16 12:31:14 +0600

    default Dic Details

--- HackerRank/DefaultDict_Tutorial.py
+++ HackerRank/DefaultDict_Tutorial.py
@@ -22,27 +22,75 @@
 
 from collections import defaultdict
 
-n, m = map(int, input().split())
+# n, m = map(int, input().split())
 
-A = []
+# A = []
+# for i in range(n):
+#     A.append(input())
+# print("A: ", A)
+
+# B = []
+# for i in range(m):
+#     B.append(input())
+# print("B: ", B)
+
+
+# d = defaultdict(list)
+
+# print("Default List: ", d)
+
+
+# for i in B:
+#     if i not in d:
+#         if i not in A:
+#             d.append[-1]
+#         else:
+#             for j in range(n):
+#                 if A[j] == i:
+#                     d[i].append(j+1)
+#     print(*d[i])
+# print(d)
+
+
+
+n = int(input())
+d = dict(list())
+
 for i in range(1, n+1):
-    A.append(input())
+    v = input()
 
-B = []
-for i in range(m):
-    B.append(input())
 
-d = defaultdict(list)
+    if v in d:
+        d[v].append(i)
+    else:
+        d[v]=[i]
+# d.[v].append(i) if we use defaultdict
 
-for i in B:
-    if i not in d: # do not append again
-        if i not in A:
-            d[i].append(-1)
 
-        else:
-            for j in range(0,n):
-                if A[j] == i:
-                    d[i].append(j+1)
+for key, val in d.items():
+    print(key, val)
 
-    print(*d[i])
-    
+
+
+
+
+
+
+
+
+
+
+# for i in B:
+#     if i not in d: # do not append again
+#         if i not in A:
+#             d[i].append(-1)
+
+#         else:
+#             for j in range(0,n):
+#                 if A[j] == i:
+#                     d[i].append(j+1)
+
+#     print(*d[i])
+
+
+
